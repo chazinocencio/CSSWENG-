542e392dada7aee18bd71fb5a81904baca774f10
deppy0 <dependant1b@gmail.com>
2026-06-03 20:04:18 +0800

    push for debugging

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import UploadDCMButton from "../components/UploadDCMButton.tsx";
 
 export default function Page() {
+	const [file, setFile] = useState<string | null>("");
+	const [error, setError] = useState<string>("");
+	const PathText = () => {
+		if (file != null && file.length < 1)
+			return null;
+		else if (file != null)
+			return <Text className="text-xl font-light text-blue-500">{file}</Text>
+		else
+			return <Text className="text-xl font-light text-red-500">Invalid DICOM file.</Text>
+	};
+	const UploadSuccess = (f) => { setFile(f); };
+	const UploadInvalid = () => { setFile(null); };
+	const UploadCancelled = () => { setFile(""); };
 	return (
 		<View className="flex-1 items-center">
 			<View className="flex-1 justify-center">
 				<Text className="text-3xl font-bold">Hello World!</Text>
 				<Text className="text-xl font-light">This is the first page of your app.</Text>
+				<PathText />
+				<UploadDCMButton
+					ButtonClass="w-[100px] h-[40px] bg-[#eb8817] justify-center items-center rounded-lg"
+					TextClass="text-xl text-white font-light"
+					ButtonText="Upload"
+					UploadSuccess={UploadSuccess}
+					UploadInvalid={UploadInvalid}
+					UploadCancelled={UploadCancelled}
+				/>
 			</View>
 		</View>
 	);
---
+++ src/components/UploadDCMButton.tsx
@@ -0,0 +1,29 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import * as edp from "expo-document-picker";
+
+export default function UploadDCMButton({UploadCancelled, UploadInvalid, UploadSuccess, ButtonText, ButtonClass, TextClass}) {
+	const GetDocument = async () => {
+		try {
+			const documents = await edp.getDocumentAsync({
+				type: 'application/dicom',
+				copyToCacheDirectory: false,
+				multiple: false,
+			});
+			if (!documents.canceled) {
+				const file = documents.assets[0];
+				return !file.name.toLowerCase().endsWith(".dcm")
+					? UploadInvalid()
+					: UploadSuccess(file);
+			} else {
+				return UploadCancelled();
+			}
+		} catch (e) {
+			return UploadInvalid(e);
+		}
+	}	
+	return (
+		<TouchableOpacity className={ButtonClass} onPress={ GetDocument }>
+			<Text className={TextClass}>{ButtonText}</Text>
+		</TouchableOpacity>
+	);
+}
